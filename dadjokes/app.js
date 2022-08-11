beeee2ead7139f6da5d768df2364fd1ffa9d5c61
i try all time <a@gmail.com>
2022-08-11 16:59:33 +0200

    'API'

---
+++ dadjokes/app.js
@@ -0,0 +1,28 @@
+const url = 'https://icanhazdadjoke.com/';
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke=async()=>{
+    try{
+        const response=await fetch(url,{
+            headers:{
+                Accept: 'application/json',
+                'User-Agent': 'learning app',
+            }
+        });
+        if(!response.ok){
+            throw new Error('error')
+        }
+        const data =await response.json()
+        result.textContent = data.joke;
+    }catch(error){
+        console.log(error.message)
+        result.textContent='there new error'
+    }
+}
+fetchDadJoke()
