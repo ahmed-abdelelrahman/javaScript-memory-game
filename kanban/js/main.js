4fe430b760412a531c0a443754dbbc73d802b8d8
i try all time <a@gmail.com>
2022-09-27 07:44:34 +0200

    'removed'

--- kanban/js/main.js
+++
@@ -1,6 +0,0 @@
-import kanbanApi from "./api/kanbanApi.js";
-import kanban from "./view/Kanban.js";
-
-new kanban(
-    document.querySelector('.kanban')
-)
--- movie-app-vuejs/movie-app/src/env.js
+++
@@ -1,3 +0,0 @@
-export default{
-    apikey:'7a3e1331'
-}
